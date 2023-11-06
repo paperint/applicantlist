889686900af7ddae11afe0877abdea073b889aa0
Nu <nuuseforstudy@gmail.com>
2023-11-07 01:33:38 +0700

    feat: added sidebar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Loading from "./components/Loading.vue";
+import DashboardLayout from "./layout/DashboardLayout.vue";
 </script>
 
 <template>
   <main class="flex flex-col items-center justify-center w-full min-h-screen">
     <Loading />
-    <RouterView />
+    <DashboardLayout>
+      <RouterView />
+    </DashboardLayout>
   </main>
 </template>
---
+++ client/src/layout/DashboardLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="drawer drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="flex flex-col items-center justify-center drawer-content">
+      <!-- Page content here -->
+      <slot></slot>
+    </div>
+    <div class="drawer-side">
+      <ul class="min-h-full p-4 menu w-80 bg-base-200 text-base-content">
+        <!-- Sidebar content here -->
+        <h2 class="mb-12 text-4xl font-bold tracking-wider text-center">
+          Dashboard
+        </h2>
+        <li class="w-full text-lg">
+          <a class="active">Job application list</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
